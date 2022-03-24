4f4e12943b001fb78efcec6f557a6bcaeba8e383
Jared Paxton <jaredpaxton56@gmail.com>
2022-03-23 23:00:52 -0600

    added initial game state types

---
+++ game/game_state.go
@@ -0,0 +1,30 @@
+package game
+
+const MaxGuesses = 6
+const NumLetters = 5
+
+type LetterStatus int
+
+const (
+	SamePosition LetterStatus = 0
+	DiffPosition LetterStatus = 1
+	NotPresent   LetterStatus = 2
+)
+
+type Guess struct {
+	Word          string
+	LettersStatus [NumLetters]LetterStatus
+}
+
+type Status int
+
+const (
+    InProgress Status = 0
+    Won Status = 1
+    Lost Status = 2
+)
+
+type GameState struct {
+	Guesses    []Guess
+    CurrStatus Status
+}
